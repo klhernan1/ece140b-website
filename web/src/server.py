b6950b46976e1608394fbd8d3044098614628d85
sepbos <sbostanb@ucsd.edu>
2022-06-07 19:54:47 -0700

    main push

--- web/src/server.py
+++ web/src/server.py
@@ -1,76 +1,73 @@
-from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
 from pyramid.response import FileResponse
-
 import mysql.connector as mysql
 import os
-
-db_user = os.environ['MYSQL_USER']
-db_pass = os.environ['MYSQL_PASSWORD']
-db_name = os.environ['MYSQL_DATABASE']
-db_host = os.environ['MYSQL_HOST']
-
-# def get_home(req):
-#   # Connect to the database and retrieve the users
-#   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-#   cursor = db.cursor()
-#   cursor.execute("select first_name, last_name, email from Users;")
-#   records = cursor.fetchall()
-#   db.close()
-
-#   return render_to_response('templates/home.html', {'users': records}, request=req)
-
-def get_home(req):
-      # Connect to the database and retrieve the users
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-
-  inst_sql = "SELECT first_name, last_name, email from Users WHERE user_role = 'instructor';"
-  ta_sql = "SELECT first_name, last_name, email from Users WHERE user_role = 'teacher assistant';"
-  team_sql = "SELECT first_name, last_name, email from Users WHERE user_role = 'team member';"
-  cursor.execute(inst_sql)
-  instructors = cursor.fetchall()
-
-  cursor.execute(ta_sql)
-  tas = cursor.fetchall()
-
-  cursor.execute(team_sql)
-  team = cursor.fetchall()
-  db.close()
-
-  return render_to_response('templates/home.html', {'users1': instructors}, {'users2': tas}, 
-                            {'users3': team}, request=req)
-
-def get_product(req):
-      
-    return FileResponse("templates/product.html")
+from wsgiref.simple_server import make_server
 
 
-def get_kvp(req):
-      
+def benefits_page(req):
+    return FileResponse("templates/benefits.html")
+
+
+def costs_page(req):
+    return FileResponse("templates/costs.html")
+
+
+def interaction_page(req):
+    return FileResponse("templates/interaction.html")
+
+
+def kvp_page(req):
     return FileResponse("templates/kvp.html")
 
 
+def product_page(req):
+    return FileResponse("templates/product.html")
 
-''' Route Configurations '''
+
+def pivot_page(req):
+    return FileResponse("templates/pivot.html")
+
+
+def revenue_page(req):
+    return FileResponse("templates/revenue.html")
+
+
+def home_page(req):
+    return FileResponse("templates/home.html")
+
+
 if __name__ == '__main__':
-  config = Configurator()
+    with Configurator() as config:
 
-  config.include('pyramid_jinja2')
-  config.add_jinja2_renderer('.html')
+        config.add_route('benefits', '/benefits')
+        config.add_view(benefits_page, route_name='benefits')
 
-  config.add_route('get_home', '/')
-  config.add_view(get_home, route_name='get_home')
+        config.add_route('costs', '/costs')
+        config.add_view(costs_page, route_name='costs')
 
-  # config.add_route('get_product', '/product')
-  # config.add_view(get_product, route_name='get_product')
+        config.add_route('home', '/')
+        config.add_view(home_page, route_name='home')
 
-  # config.add_route('get_kvp', '/kvp')                 
-  # config.add_view(get_kvp, route_name='get_kvp')
+        config.add_route('interaction', '/interaction')
+        config.add_view(interaction_page,
+                        route_name='interaction')
 
-  config.add_static_view(name='/', path='./public', cache_max_age=3600)
+        config.add_route('kvp', '/kvp')
+        config.add_view(kvp_page, route_name='kvp', renderer='json')
 
-  app = config.make_wsgi_app()
-  server = make_server('0.0.0.0', 6000, app)
-  server.serve_forever()
+        config.add_route('pivot', '/pivot')
+        config.add_view(pivot_page, route_name='pivot')
+
+        config.add_route('product', '/product')
+        config.add_view(product_page, route_name='product', renderer='json')
+
+        config.add_route('revenue', '/revenue')
+        config.add_view(revenue_page,
+                        route_name='revenue')
+
+        config.add_static_view(name='/', path='./public', cache_max_age=3600)
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6000, app)
+    server.serve_forever()
